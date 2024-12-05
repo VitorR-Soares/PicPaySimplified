84a372b10f71967752ceae7ebe263dd981d380f0
Vitor Soares <vitorratollasoares@gmail.com>
2024-12-05 12:24:24 -0300

    First Commit

---
+++ PicPaySimplified/src/main/java/com/ratolla/PicPaySimplified/PicPaySimplifiedApplication.java
@@ -0,0 +1,13 @@
+package com.ratolla.PicPaySimplified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPaySimplifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPaySimplifiedApplication.class, args);
+	}
+
+}
